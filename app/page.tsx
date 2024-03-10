8cc162d86967250b209db0d6397a3c9d1e8ac1b8
Esther <elvynnn@gmail.com>
2024-03-11 03:20:20 +0900

    Fixed styling with topbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@/components/sidebar/sidebar"
 
 export default function Home() {
   return (
-    <main className="h-full">
+    <main className="h-full pt-[58px]">
       <div className="flex flex-wrap flex-row gap-5 h-full">
         <Sidebar />
         <div className="grow">main view</div>
