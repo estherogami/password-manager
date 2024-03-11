f6e49eb8d3faaf352681ffc24c08eef991756671
Esther <elvynnn@gmail.com>
2024-03-11 22:21:49 +0900

    refactor code for reusability

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -2,7 +2,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
+import { formSchema } from "@/schemas/project"
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -28,30 +28,6 @@ import Link from "next/link"
 import { BsImage } from "react-icons/bs";
 
 
-//File input validation variables
-const MAX_FILE_SIZE = 500000;
-const ACCEPTED_IMAGE_MIME_TYPES = [
-  "image/jpeg",
-  "image/jpg",
-  "image/png",
-  "image/webp",
-];
-
-//Form Schema
-const formSchema = z.object({
-  title: z.string().min(3, { message: "Tittle must be longer than 3 characters" }).max(50, { message: "Title must be shorter than 50 characters." }),
-  url: z.union([z.literal(""), z.string().trim().url()]),
-  adImage: z
-    .any()
-    .refine((files) => {
-      return files?.[0]?.size <= MAX_FILE_SIZE;
-    }, `Max image size is 5MB.`)
-    .refine(
-      (files) => ACCEPTED_IMAGE_MIME_TYPES.includes(files?.[0]?.type),
-      "Only .jpg, .jpeg, .png and .webp formats are supported."
-    ),
-
-});
 
 
 function NewProjectPage() {
